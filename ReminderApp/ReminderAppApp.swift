3fb7703c00241a60d9459599788f8a519afa0ead
Hunter <hunterweisenbach@me.com>
2026-01-08 16:14:40 -0500

    initial commit

---
+++ ReminderApp/ReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderAppApp.swift
+//  ReminderApp
+//
+//  Created by Hunter Weisenbach on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct ReminderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
